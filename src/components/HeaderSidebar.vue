82020e672e343c3c00abbc12151bc1257643fecf
Alsajet <a.molla@gruppointent.com>
2024-10-22 15:58:04 +0200

    travaso

---
+++ src/components/HeaderSidebar.vue
@@ -0,0 +1,85 @@
+<template>
+  <div :class="`contact-sidebar ${show ? ' active' : ''}`">
+    <div class="contact-box">
+      <div class="logo-contact logo-dark">
+        <RouterLink to="?"
+          ><img class="header_logo" src="@/assets/images/TH-6.jpg" alt=""
+        /></RouterLink>
+      </div>
+      <div class="m-b50 contact-text">
+        <div class="dz-title">
+          <h4>Chi siamo</h4>
+          <div class="dz-separator style-1 text-primary mb-0"></div>
+        </div>
+         <RouterLink to="/about-us" class="btn btn-primary btn-sm btn-rounded">READ MORE</RouterLink>
+      </div>
+      <div class="dz-title">
+        <h4>I Nostri Servizi</h4>
+        <div class="dz-separator style-1 text-primary mb-0"></div>
+      </div>
+      <RouterLink to="/pratiche-urbanistiche" class="btn btn-primary btn-sm btn-rounded">Pratiche Urbanistiche</RouterLink>
+      <h2></h2>
+      <RouterLink to="/pratiche-catastali" class="btn btn-primary btn-sm btn-rounded">Pratiche Catastali</RouterLink>
+      <h2></h2>
+      <RouterLink to="/perizie-stime-tribunale" class="btn btn-primary btn-sm btn-rounded">Perizie, Stime e Tribunale</RouterLink>
+      <h2></h2>
+      <RouterLink to="/investimenti-immobiliari" class="btn btn-primary btn-sm btn-rounded">Investimenti Immobiliari</RouterLink>
+      <h2></h2>
+      <RouterLink to="/edilizia" class="btn btn-primary btn-sm btn-rounded">Edilizia</RouterLink>
+      <h2></h2>
+      <RouterLink to="/servizi-vari" class="btn btn-primary btn-sm btn-rounded">Servizi Vari</RouterLink>
+      <!-- <div class="widget bg-white widget_gallery" style="z-index: 99999">
+        <Lightgallery
+          :settings="{ speed: 500, plugins: plugins, selector: '.lightimg' }"
+          class="lightgallery m-b0"
+        >
+          <li v-for="({ img }, ind) in GalleryBlog" :key="ind">
+            <div class="dlab-post-thum dlab-img-effect">
+              <span :data-src="img" class="lightimg" title="Image 1 Title will come here">
+                <img :src="img" alt="" />
+              </span>
+            </div>
+          </li>
+        </Lightgallery>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+import Lightgallery from 'lightgallery/vue'
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgZoom from 'lightgallery/plugins/zoom'
+import service_pic1 from '@/assets/images/services/pic1.jpg'
+import service_pic2 from '@/assets/images/services/pic2.jpg'
+import service_pic3 from '@/assets/images/services/pic3.jpg'
+import service_pic4 from '@/assets/images/services/pic4.jpg'
+import service_pic5 from '@/assets/images/services/pic5.jpg'
+import service_pic6 from '@/assets/images/services/pic6.jpg'
+
+export default defineComponent({
+  props: {
+    show: {
+      type: Boolean as PropType<boolean>,
+      required: true
+    }
+  },
+  components: { Lightgallery },
+  data: () => ({
+    plugins: [lgThumbnail, lgZoom]
+  }),
+  // setup() {
+  //   return {
+  //     GalleryBlog: [
+  //       { img: service_pic1 },
+  //       { img: service_pic2 },
+  //       { img: service_pic3 },
+  //       { img: service_pic4 },
+  //       { img: service_pic5 },
+  //       { img: service_pic6 }
+  //     ]
+  //   }
+  // }
+})
+</script>
---
+++ src/components/HomeAbout.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <!-- <div class="dz-media"> -->
+          <!-- <div class="img1 aos-item">
+            <img src="@/assets/images/about/pic13.jpg" alt="" />
+          </div>
+          <div class="img2 aos-item">
+            <img src="@/assets/images/about/pic14.jpg" alt="" />
+          </div> -->
+          <div class="img3 aos-item me-5">
+            <img src="@/assets/images/about/pic16.jpg" alt="" />
+          </div>
+        <!-- </div> -->
+      </div>
+      <div class="col-lg-6 align-self-center">
+        <div class="year-exp">
+          <h2 class="year aos-item counter">15</h2>
+          <h4 class="text aos-item">Anni di esperienza lavorativa</h4>
+        </div>
+        <p class="m-b30 aos-item">
+          Con oltre 15 anni di esperienza nel settore immobiliare,
+          la nostra società si distingue per la professionalità e l'affidabilità nella compravendita di immobili.
+          Specializzati nell'intermediazione di case residenziali e immobili commerciali,
+          offriamo soluzioni su misura per soddisfare le esigenze di ogni cliente. Inoltre,
+          ci occupiamo di investimenti immobiliari, aiutando privati e aziende a identificare le migliori opportunità sul mercato.
+          Il nostro team di esperti fornisce anche servizi di progettazione e consulenza edilizia,
+          garantendo la massima qualità in ogni fase del progetto.
+          Grazie alla nostra esperienza e alla nostra rete di contatti consolidata,
+          siamo in grado di gestire ogni aspetto della transazione immobiliare,
+          assicurando ai nostri clienti un servizio completo e personalizzato.
+          Affidati a noi per realizzare il tuo progetto immobiliare con successo e serenità.
+        </p>
+        <ul>
+    <li style="margin-top: 10px;">
+        <h6 style="display: inline; margin-right: 10px;">Titolare:</h6>
+        <span>Luca Procesi</span>
+    </li>
+    <li style="margin-top: 10px;">
+        <h6 style="display: inline; margin-right: 10px;">Responsabile Amministrativo:</h6>
+        <span>Marika Grimaldi</span>
+    </li>
+    <li style="margin-top: 10px;">
+        <h6 style="display: inline; margin-right: 10px;">Coordinamento Pratiche Urbanistiche:</h6>
+        <span>Cristina del prete</span>
+    </li>
+    <li style="display: flex; align-items: flex-start; margin-top: 10px;">
+        <h6 style="margin-right: 10px;">Consulenti:</h6>
+        <div style="display: flex; flex-direction: column; align-items: flex-start;">
+            <span>Pierluigi Minna</span>
+            <span>Omar Verducci</span>
+            <span>Luigi D'Arienzo</span>
+            <span>Valerio Bonafede</span>
+        </div>
+    </li>
+</ul>
+
+
+
+        <!-- <div class="accordion dz-accordion about-faq" id="aboutFaq">
+          <div class="accordion-item">
+            <h4 class="accordion-header">
+              <RouterLink
+                to="?"
+                class="accordion-button"
+                data-bs-toggle="collapse"
+                data-bs-target="#aboutOne"
+                aria-expanded="true"
+                aria-controls="aboutOne"
+              >
+                <i class="flaticon-crane"></i>
+                Construction
+                <span class="toggle-close"></span>
+              </RouterLink>
+            </h4>
+            <div
+              id="aboutOne"
+              class="accordion-collapse collapse show"
+              aria-labelledby="aboutOne"
+              data-bs-parent="#aboutFaq"
+            >
+              <div class="accordion-body">
+                <p class="m-b0">
+                  Vivamus sed eleifend diam. Proin vel orci commodo nulla viverra feugiat nec quis
+                  neque.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h4 class="accordion-header">
+              <RouterLink
+                to="?"
+                class="accordion-button collapsed"
+                data-bs-toggle="collapse"
+                data-bs-target="#aboutTwo"
+                aria-expanded="false"
+                aria-controls="aboutTwo"
+              >
+                <i class="flaticon-interior-design"></i>
+                Architecture
+                <span class="toggle-close"></span>
+              </RouterLink>
+            </h4>
+            <div
+              id="aboutTwo"
+              class="accordion-collapse collapse"
+              aria-labelledby="aboutTwo"
+              data-bs-parent="#aboutFaq"
+            >
+              <div class="accordion-body">
+                <p class="m-b0">
+                  Vivamus sed eleifend diam. Proin vel orci commodo nulla viverra feugiat nec quis
+                  neque.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <h4 class="accordion-header">
+              <RouterLink
+                to="?"
+                class="accordion-button collapsed"
+                data-bs-toggle="collapse"
+                data-bs-target="#aboutThree"
+                aria-expanded="false"
+                aria-controls="aboutTwo"
+              >
+                <i class="flaticon-blueprint"></i>
+                Interior
+                <span class="toggle-close"></span>
+              </RouterLink>
+            </h4>
+            <div
+              id="aboutThree"
+              class="accordion-collapse collapse"
+              aria-labelledby="aboutThree"
+              data-bs-parent="#aboutFaq"
+            >
+              <div class="accordion-body">
+                <p class="m-b0">
+                  Vivamus sed eleifend diam. Proin vel orci commodo nulla viverra feugiat nec quis
+                  neque.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterLink } from 'vue-router'
+
+export default defineComponent({
+  name: 'homeAbout',
+  setup() {
+    return {}
+  },
+  components: { RouterLink }
+})
+</script>
+
+<style scoped></style>
