a382393d4423700d5eda0a5a82822a7af65ae439
Alsajet <a.molla@gruppointent.com>
2025-01-09 16:01:13 +0100

    scrollpage2

--- src/elements/BlogPagination.vue
+++ src/elements/BlogPagination.vue
@@ -2,13 +2,13 @@
   <nav aria-label="Blog Pagination">
     <ul class="pagination text-center m-b30">
       <li class="page-item">
-        <button v-if="currentPage! > 1" class="page-link prev" @click="$emit('changePage', currentPage! - 1, filter, typologie, location, code, from, to)"><i class="la la-angle-left"></i></button>
+        <button v-if="currentPage! > 1" class="page-link prev" @click="newPageClick(currentPage! - 1)"><i class="la la-angle-left"></i></button>
       </li>
       <li v-for="page in totalPages" :key="page" class="page-item">
-        <button class="page-link" :class="currentPage! == page ? 'active' : ''" @click="$emit('changePage', page, filter, typologie, location, code, from, to)">{{ page }}</button>
+        <button class="page-link" :class="currentPage! == page ? 'active' : ''" @click="newPageClick(page)">{{ page }}</button>
       </li>
       <li v-if="currentPage! < totalPages!" class="page-item">
-        <button class="page-link next" @click="$emit('changePage', currentPage! + 1, filter, typologie, location, code, from, to)"><i class="la la-angle-right"></i></button>
+        <button class="page-link next" @click="newPageClick(currentPage! + 1)"><i class="la la-angle-right"></i></button>
       </li>
     </ul>
   </nav>
@@ -40,7 +40,7 @@ export default defineComponent({
   },
   methods: {
   newPageClick(newpage: number) {
-    this.$emit('changePage', newpage, this.filter, this.typologie);
+    this.$emit('changePage', newpage, this.filter, this.typologie, this.location, this.code, this.from, this.to);
     window.scrollTo({
       top: 920,          
       behavior: 'smooth' 
